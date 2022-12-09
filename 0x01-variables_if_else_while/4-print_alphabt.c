8ad3328218b4fe975cf008fe5ea62042cff3e353
Peter <peter.imboo@gmail.com>
2022-12-09 07:57:28 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c l= 'e' && c l= 'q')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
