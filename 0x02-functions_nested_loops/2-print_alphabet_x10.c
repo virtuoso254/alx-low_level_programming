6fec148c2a4b8acc593ca8aff4e24ced04e9a6a3
Peter <peter.imboo@gmail.com>
2022-12-14 01:56:14 -0800

    task two

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char y;
+	int i = 1;
+
+	while (i <= 10)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
