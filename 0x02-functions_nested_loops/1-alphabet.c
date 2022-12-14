352b9ed9b3c6e98a98c4c00eb179bde5fa5ed95b
Peter <peter.imboo@gmail.com>
2022-12-14 01:13:39 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - outputs the alphabets
+ * Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char y;
+
+	for (y = 'a'; y <= 'z'; y++)
+	{
+		_putchar(y);
+	}
+	_putchar('\n');
+}
