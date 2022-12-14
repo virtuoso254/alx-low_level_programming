9f591442e1775c577755cec17df2a951b896ab6c
Peter <peter.imboo@gmail.com>
2022-12-14 07:14:08 -0800

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _abs - computes absolute values of integers
- * @n: the number computed
+ * _abs - computes integer
+ * @c: the number computed
  * Return: 0 success
 */
 int _abs(int c)
@@ -10,6 +10,7 @@ int _abs(int c)
 	if (c < 0)
 	{
 		int abs_val;
+
 		abs_val = c * -1;
 		return (abs_val);
 	}
