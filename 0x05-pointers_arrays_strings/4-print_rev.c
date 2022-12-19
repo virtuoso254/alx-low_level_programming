042aa8aa68e256f5c8a20c1a73e4cdb4a1db046d
Peter <peter.imboo@gmail.com>
2022-12-19 05:42:38 -0800

    task four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses rthe characters
+ * @s: input
+ * Rerurn: nil
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
