487232be045a6d8e40cd67b7c83435a2ee916e6d
virtuoso254 <peter.imboo@gmail.com>
2023-01-10 02:43:16 -0800

    lists of arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints commandline arguments
+ * @argc: arguments counts
+ * @argv: arguments list
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
