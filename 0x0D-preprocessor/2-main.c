c7d36ca4d166ae0157854dbb6acb1d7094d7f012
virtuoso254 <peter.imboo@gmail.com>
2023-01-17 03:20:34 -0800

    current file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - outputs the name of the file
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
