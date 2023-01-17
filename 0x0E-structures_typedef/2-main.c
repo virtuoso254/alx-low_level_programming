d3f3247a59edcfea30e7ad57cc4c2d9ef803ffeb
virtuoso254 <peter.imboo@gmail.com>
2023-01-17 05:05:53 -0800

    unconditional love equals good life

---
+++ 0x0E-structures_typedef/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - checks the code
+ * Return: always 0
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
+	return (0);
+}
