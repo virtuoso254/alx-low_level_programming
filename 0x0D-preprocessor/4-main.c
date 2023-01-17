62e239649c5b7a0894794b47a361c67573a103f6
virtuoso254 <peter.imboo@gmail.com>
2023-01-17 03:30:42 -0800

    summation

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - checks the code
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+
+}
