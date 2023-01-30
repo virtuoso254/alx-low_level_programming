0a6741b84a1e87a6c57a3bff7e8e875b4d8afb8d
virtuoso254 <peter.imboo@gmail.com>
2023-01-30 01:29:11 -0800

    outputs all elements in a list

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+project of singly linked lists
---
+++ pedro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * Return: always 0 on success
+ */
+
+int main(void)
+{
+	printf("hellow world");
+	return (0);
+}
+
