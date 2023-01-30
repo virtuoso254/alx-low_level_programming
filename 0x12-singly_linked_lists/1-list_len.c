f4cd9f4e87d242143afc26de5d835b44fa039702
virtuoso254 <peter.imboo@gmail.com>
2023-01-30 01:38:41 -0800

    elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- * list_len - outputs number nodes
+ * list_len - returns then number of elements in a list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t num_of_elements
+	size_t number_of_nodes;
 
-	num_of_elements = 0;
+	number_of_nodes = 0;
 	while (h != NULL)
 	{
 		h = h->next;
-		num_of_elements++;
+		number_of_nodes++;
 	}
-	return (num_of_elements);
-}
+	return (number_of_nodes);
 
