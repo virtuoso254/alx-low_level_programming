686e77929bc659be97653b2054d650782cf99796
virtuoso254 <peter.imboo@gmail.com>
2023-01-30 02:06:15 -0800

    addition of nodes

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * _strlen - outputs length of the string
+  * @s: string to be checked
+  * Return: length of the string
+  */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+  * add_node - adds a node to the beginning
+  * @head: beginning of the node
+  * @str: string to add to the head
+  * Return: pointe.
+  */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i, len;
+	char *content;
+	list_t *new;
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+	if (content == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
